98708edf7bee50295720c540b52bc4bc69e45541
Alex Phelps <alexphelps3@gmail.com>
2023-01-26 14:06:32 +0700

    fix build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,19 +26,23 @@ function HomepageHeader() {
   );
 }
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
-}
+// export default function Home() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <Layout
+//       title={`Hello from ${siteConfig.title}`}
+//       description="Description will go into a meta tag in <head />">
+//       <HomepageHeader />
+//       <main>
+//         <HomepageFeatures />
+//       </main>
+//     </Layout>
+//   );
+// }
 
-// temp disable homepage while waiting for design.
-window.location = '/docs/'
+// import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+export default function Home() {
+    return <Redirect to='/docs/' />;
+}
