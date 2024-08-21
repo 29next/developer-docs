48d8b2333a2fb2040c2c52b42f669fe0ed5ffa40
Alex Phelps <alexphelps3@gmail.com>
2024-08-21 14:56:24 +0700

    add search indexing for campaigns api endpoints and webhooks

--- tools/update_search.py
+++ tools/update_search.py
@@ -7,58 +7,88 @@ BASE_API_FILES_PATH = '../static/api/'
 app_id = 'GNSJUJD786'
 api_key = os.environ['ALGOLIA_API_KEY']
 index_name = 'docs'
-api_base_url = 'https://developers.29next.com/docs/api/admin/reference/#/operations/'
+domain = 'https://developers.29next.com'
 
 
-def generate_objects():
+def create_index_object(api_type, tag, object_id, description, anchor):
+    url = domain + anchor
+
+    navigation = {
+        'lvl0': 'Documentation',
+        'lvl1': '{} API'.format(api_type.capitalize()),
+        'lvl2': None,
+        'lvl3': tag,
+        'lvl4': object_id,
+        'lvl5': description,
+        'lvl6': None
+    }
+    object = {
+        'objectID': object_id,
+        'name': object_id,
+        'content': description,
+        'docusaurus_tag': 'docs-default-current',
+        'category': 'API',
+        'language': 'en',
+        'type': 'lvl4',
+        'anchor': anchor,
+        'url': url,
+        'url_without_variables': url,
+        'url_without_anchor': url,
+        'weight': {
+            'page_rank': 100,
+            'level': 100,
+            'position': 1
+        },
+        'hierarchy': navigation,
+        'hierarchy_radio': navigation,
+        'hierarchy_camel': [navigation],
+        'hierarchy_radio_camel': navigation
+    }
+
+    return object
+
+
+def generate_api_objects(api_type, version):
+    print('Generating {} api index objects...'.format(api_type))
     apis = []
 
-    type = 'admin'
-    version = '2024-04-01'
-    api_file = BASE_API_FILES_PATH + '/{}/{}.yaml'.format(type, version)
+    api_file = BASE_API_FILES_PATH + '/{}/{}.yaml'.format(api_type, version)
+
     with open(api_file, 'r+') as f:
         spec = yaml.safe_load(f.read())
         for path, methods in spec['paths'].items():
 
             for each in methods.items():
-                operation_id = each[1].get('operationId', '')
+                object_id = each[1].get('operationId', '')
                 tag = each[1]['tags'][0]
                 description = each[1].get('description', '')
-                url = '{}{}{}'.format(api_base_url, operation_id, '?ref=search')
-                navigation = {
-                        'lvl0': 'Documentation',
-                        'lvl1': 'Admin API',
-                        'lvl2': None,
-                        'lvl3': tag,
-                        'lvl4': operation_id,
-                        'lvl5': description,
-                        'lvl6': None
-                }
+                anchor = '/docs/api/{}/reference/?v={}#/operations/{}'.format(api_type, version, object_id)
                 apis.append(
-                    {
-                        'objectID': operation_id,
-                        'name': operation_id,
-                        'content': description,
-                        'docusaurus_tag': 'docs-default-current',
-                        'category': 'API',
-                        'language': 'en',
-                        'type': 'lvl4',
-                        'anchor': '/operations/{}'.format(operation_id),
-                        'url': url,
-                        'url_without_variables': url,
-                        'url_without_anchor': url,
-                        'weight': {
-                            'page_rank': 100,
-                            'level': 100,
-                            'position': 1
-                        },
-                        'hierarchy': navigation,
-                        'hierarchy_radio': navigation,
-                        'hierarchy_camel': [navigation],
-                        'hierarchy_radio_camel': navigation
-                    }
+                    create_index_object(api_type, tag, object_id, description, anchor)
                 )
     return apis
+
+
+def generate_webhook_objects():
+    print('Generating webhook index objects...')
+    webhooks = []
+
+    api_type = 'admin'
+    version = '2024-04-01'
+    api_file = BASE_API_FILES_PATH + '/{}/{}.yaml'.format(api_type, version)
+
+    with open(api_file, 'r+') as f:
+        spec = yaml.safe_load(f.read())
+        for webhook in spec['webhooks'].items():
+            tag = webhook[1]['post']['tags'][0]
+            object_id = webhook[0]
+            description = webhook[1]['post']['description']
+            anchor = '/docs/api/admin/reference/?v={}#/webhooks/{}/post'.format(version, object_id)
+            webhooks.append(
+                create_index_object('admin', tag, object_id, description, anchor)
+            )
+    return webhooks
+
 
 
 def add_to_index():
@@ -66,8 +96,25 @@ def add_to_index():
     client = SearchClient.create(app_id, api_key)
     index = client.init_index(index_name)
 
-    objects = generate_objects()
+    indexed_apis = [
+        {
+            'type': 'admin',
+            'version': '2024-04-01'
+        },
+        {
+            'type': 'campaigns',
+            'version': 'v1'
+        }
+    ]
 
-    index.save_objects(objects)
+    for each in indexed_apis:
+        objects = generate_api_objects(each['type'], each['version'])
+        print('Pushing {} objects to search index...'.format(len(objects)))
+        index.save_objects(objects)
+
+    webhook_objects = generate_webhook_objects()
+    print('Pushing webhook objects to search index...'.format(len(webhook_objects)))
+    index.save_objects(webhook_objects)
+
 
 add_to_index()
