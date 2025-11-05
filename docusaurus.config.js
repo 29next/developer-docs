1e9632803f61aae9cc754f3933a24a3235dfca0d
Alex Phelps <alexphelps3@gmail.com>
2025-11-05 14:20:44 +0700

    update algolia config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -199,7 +199,7 @@ const config = {
             algolia: {
                 appId: 'WDDPJKSDHF',
                 apiKey: '4299e5ff1b1a1278c60158fbba4d65c1',
-                indexName: 'docs',
+                indexName: 'Developer Docs Crawler',
                 askAi: 'YOUR_ALGOLIA_ASSISTANT_ID', 
                 // contextualSearch: false,
                 // searchParameters: {},
